5fac486ab7d9781944a8048c2ea6a0ddb0303e00
Kenny Cho <Kenny.Cho@softwire.com>
2024-06-25 11:11:31 +0100

    Refactored 3 & 5 and added rules for 7, 11, 13, 17

--- app/src/main/java/com/example/fizzbuzz/fizzbuzz.kt
+++ app/src/main/java/com/example/fizzbuzz/fizzbuzz.kt
@@ -1,17 +1,70 @@
 package com.example.fizzbuzz
 
+fun addWord(words: MutableList<String>, word: String) {
+    words.add(word)
+}
+
+fun setWordsToBong(words: MutableList<String>) {
+    words.clear()
+    words.add("Bong")
+}
+
+fun insertFezz(words: MutableList<String>) {
+    var inserted = false
+    for (i in words.indices) {
+        if (words[i][0] == 'B') {
+            words.add(i, "Fezz")
+            inserted = true
+            break
+        }
+    }
+    if (!inserted) {
+        words.add("Fezz")
+    }
+}
+
+fun reverseWords(words: MutableList<String>) {
+    words.reverse()
+}
+
+fun printNumOrWords(num: Int, words: MutableList<String>) {
+    var combined = ""
+    if (words.isEmpty()) {
+        println(num)
+    } else {
+        for (word in words) {
+            combined += word
+        }
+        println(combined)
+    }
+}
+
 fun main() {
-    println("Hello world!")
+    val words = mutableListOf<String>()
 
     for (num in 1..100) {
-        if (num % 3 == 0 && num % 5 == 0) {
-            println("FizzBuzz")
-        } else if (num % 3 == 0) {
-            println("Fizz")
-        } else if (num % 5 == 0) {
-            println("Buzz")
-        } else {
-            println(num)
+        if (num % 3 == 0) {
+            addWord(words, "Fizz")
         }
+        if (num % 5 == 0) {
+            addWord(words, "Buzz")
+        }
+        // print "FizzBuzzBang" for 3*5*7 = 105?
+        if (num % 7 == 0) {
+            addWord(words, "Bang")
+        }
+        if (num % 11 == 0) {
+            setWordsToBong(words)
+        }
+        if (num % 13 == 0) {
+            insertFezz(words)
+        }
+        if (num % 17 == 0) {
+            reverseWords(words)
+        }
+
+        printNumOrWords(num, words)
+        words.clear()
+
     }
 }
